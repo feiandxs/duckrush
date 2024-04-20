3c06cf2c346c03562578417c64bffd773cf17f03
feiandxs <yiziyint@gmail.com>
2024-04-20 19:05:18 +0800

    network doesn't support duckduckgo ,save it

--- src/handler/search-request.ts
+++ src/handler/search-request.ts
@@ -4,6 +4,16 @@ import type { SearchRequestType } from '@/schema/search.schema';
 
 const searchHandler = async (request: SearchRequestType) => {
   console.log(12312313);
+  try {
+    const serachResults = await search(request.query);
+    console.log(serachResults);
+    console.log('----------------');
+  } catch (err: any) {
+    console.error(err);
+    console.log('==============');
+  }
+
+  return '132';
 };
 
 export { searchHandler };
